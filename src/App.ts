6ab6f99a66b27e038cc82c24db7c4d08afff71ca
Jørgen H. Brække <jorgen.brakke@capgemini.com>
2021-01-05 09:48:34 +0100

    Changed build folder

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,19 @@
 import * as express from 'express';
 class App {
-    
   public express;
+
   constructor() {
     this.express = express();
     this.mountRoutes();
   }
+
   private mountRoutes(): void {
     const router = express.Router();
     router.get('/', (req, res) => {
-      res.json({ message: 'Go away, world!' });
+      res.json({ message: 'Hello, I am your first router!' });
     });
     this.express.use('/', router);
   }
 }
+
 export default new App().express;
