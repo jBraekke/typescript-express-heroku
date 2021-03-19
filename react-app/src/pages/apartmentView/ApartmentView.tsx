e454ea13a0cf0cde237770b0770031a101c2a1a1
Robert <55789494+rabjaert@users.noreply.github.com>
2021-03-19 14:48:09 +0100

    Styled the page, not responsive yet

--- react-app/src/pages/apartmentView/ApartmentView.tsx
+++ react-app/src/pages/apartmentView/ApartmentView.tsx
@@ -4,12 +4,92 @@ import { useHistory, useParams, NavLink } from 'react-router-dom';
 import Grid from "@material-ui/core/Grid";
 import { useFetch } from "../../hooks/useFetch";
 import { IApartment } from "../../interfaces/IApartment";
-import { Card, CardMedia, Container } from "@material-ui/core";
+import { Box, Button, Card, CardContent, CardMedia, Container, Divider, Typography } from "@material-ui/core";
+import theme from "../../themes/theme";
+
 //import CarCard from "../../components/cards/CarCard";
 //import { Button, Input } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: { flexGrow: 1 },
+
+  headerAddress: {
+      marginTop: theme.spacing(2),
+  },
+  informationText: {
+    //marginLeft: theme.spacing(5),
+    marginTop: theme.spacing(1),
+  },
+
+  secondInformationText: {
+    float: "right",
+    marginRight: theme.spacing(20),
+    fontWeight: "bold",
+  },
+  container: {
+    //textAlign: "center",
+    marginTop: theme.spacing(2),
+  },
+  button: {
+  backgroundColor: theme.palette.primary.main,/* Green */
+  border: "none",
+  borderRadius: "15px",
+  textAlign: "center",
+  textDecoration: "none",
+  fontSize: "20px",
+  fontFamiliy: "Garamond, serif",
+  fontWeight: "bold",
+  color: "lightgray",
+  
+  "&:hover": {
+   
+    opacity: "80%",
+    textUnderlineOffset: "30px",
+    backgroundColor: theme.palette.primary.main,
+  },
+
+  },
+
+  buttonSlett: {
+    backgroundColor:  "#fa4343",
+    border: "none",
+    borderRadius: "15px",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "20px",
+    fontFamiliy: "Garamond, serif",
+    fontWeight: "bold",
+    color: "white",
+    
+    "&:hover": {
+     
+      opacity: "80%",
+      textUnderlineOffset: "30px",
+      backgroundColor:  "#fa4343",
+    },
+  },
+
+  buttonRediger: {
+    backgroundColor:  "#abd32e",
+    border: "none",
+    borderRadius: "15px",
+    textAlign: "center",
+    textDecoration: "none",
+    fontSize: "20px",
+    fontFamiliy: "Garamond, serif",
+    fontWeight: "bold",
+    color: "white",
+    
+    "&:hover": {
+     
+      opacity: "80%",
+      backgroundColor:  "#abd32e",
+    },
+  },
+
+
+
+
 });
 
 const ApartmentView = () => {
@@ -57,48 +137,80 @@ const ApartmentView = () => {
 
 
   const CornRow = () => {
-    return <>
+    return (
+        <>
+      
+      <Grid item xs={12}>
+      <Typography variant="h2" component="h2" className={classes.headerAddress}>{realEstate?.address}</Typography>
+      <Typography variant="h6" component="h6">{realEstate?.city}</Typography>
+      <br/>
+      <Divider />
+      </Grid>
     
-        {realEstate?.imagePath ? (
-          <>
-            <CardMedia
-              component="img"
-              height="300"
-              alt="Apartment"
-              src={"./uploads/" + realEstate?.imagePath}
-              title="Apartment"
-            />
-          </>
-        ) : (
-          <>
-            <CardMedia
-              component="img"
-              height="300"
-              alt="Apartment2"
-              src="public/hus.jpg"
-              title="Apartment"
-            />
-          </>
-        )}
-    
+      <Grid item xs={12}>
+     <Typography variant="h4" component="h4" className={classes.headerAddress}>Oversikt</Typography>
+     
+      </Grid>
 
-      <Grid container item xs={12}>
-    <Grid item xs={12}>
-      <p>{realEstate?.title}</p>
-      <p>{realEstate?.address}</p>
-      <p>{realEstate?.description}</p>
-      <p>{realEstate?.city}</p>
-      </Grid>
-      </Grid>
+      <Grid container item xs={12} spacing={1} className={classes.container}>
+            
+            <Grid item xs={4}>
+            
+            <Typography variant="h6" component="h6" className={classes.informationText}>Depositum: <span className={classes.secondInformationText}>{realEstate?.deposit}</span> </Typography>
+            <Typography variant="h6" component="h6" className={classes.informationText}>Husleiegaranti: <span className={classes.secondInformationText}>{realEstate?.rentGuarantee}</span> </Typography>
+            <Typography variant="h6" component="h6" className={classes.informationText}>Mulighet for parkering: <span className={classes.secondInformationText}>{realEstate?.parking}</span> </Typography>
+            </Grid>
+            
+            <Grid item xs={4}>
+            <Typography variant="h6" component="h6" className={classes.informationText}>Kvadratmeter: <span className={classes.secondInformationText}>{realEstate?.squareMeter}</span> </Typography>
+            <Typography variant="h6" component="h6" className={classes.informationText}>Antall soverom: <span className={classes.secondInformationText}>{realEstate?.bedrooms}</span> </Typography>
+            <Typography variant="h6" component="h6" className={classes.informationText}>Antall bad: <span className={classes.secondInformationText}>{realEstate?.bathrooms}</span> </Typography>
+            </Grid>
+            
+          <Grid item xs={4}>
+          <Card>
+          
+        <CardContent>
+        <Typography
+          gutterBottom
+          variant="h4"
+          align={"center"}
+          component="h2"
+          
+        >Kontakt oss om leilighet</Typography>
+        <br/>
+
+      <Typography variant="h6" component="h6" className={classes.informationText}>Månedsleie: <span className={classes.secondInformationText}>{realEstate?.price}</span> </Typography>
+        <br/>
+        <Button className={classes.button}>Send oss en mail</Button>
+        <br/>
+        <Button className={classes.buttonRediger}>Rediger annonse</Button>
+        <br/>
+        <Button className={classes.buttonSlett}>Slett annonse</Button>
+
+        </CardContent>
       
-      
-      
-      
-      
-      
-      
-      </>;
-  };
+       </Card>
+            </Grid>
+            </Grid>
+            <Divider/>
+
+        
+         
+          <Grid container item xs={12} className={classes.container}>
+            
+            <Grid item xs={6}>
+            <Typography variant="h2" component="h2" className={classes.headerAddress}>{realEstate?.title}</Typography>
+            <Typography variant="h6" component="h6" className={classes.informationText}>{realEstate?.description}</Typography>
+
+            </Grid>
+          
+            </Grid>
+
+
+    </>
+  );
+};
 
   return (
     <div className={classes.root}>
