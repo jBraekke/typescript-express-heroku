a914b0062c6e5c07635c44765cd51f21b007b45e
Robert <55789494+rabjaert@users.noreply.github.com>
2021-03-22 15:02:32 +0100

    Update ApartmentView.tsx

--- react-app/src/pages/apartmentView/ApartmentView.tsx
+++ react-app/src/pages/apartmentView/ApartmentView.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useHistory, useParams, NavLink } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import Grid from "@material-ui/core/Grid";
 import { useFetch } from "../../hooks/useFetch";
 import { IApartment } from "../../interfaces/IApartment";
-import { Box, Button, Card, CardContent, CardMedia, Container, Divider, Typography } from "@material-ui/core";
+import { Box, Button, Card, CardContent, CardMedia, Container, Divider, Typography, useMediaQuery } from "@material-ui/core";
 import theme from "../../themes/theme";
+import hus from "../../../public/hus.jpg";
 
 //import CarCard from "../../components/cards/CarCard";
 //import { Button, Input } from "@material-ui/core";
@@ -26,6 +27,17 @@ const useStyles = makeStyles({
     marginRight: theme.spacing(20),
     fontWeight: "bold",
   },
+  secondInformationTextMobile: {
+    
+    marginLeft: theme.spacing(7),
+    fontWeight: "bold",
+  },
+
+  cardContentMobile: {
+      marginTop: theme.spacing(10),
+  },
+
+
   container: {
     //textAlign: "center",
     marginTop: theme.spacing(2),
@@ -136,10 +148,86 @@ const ApartmentView = () => {
   const classes = useStyles();
 
 
-  const CornRow = () => {
+  const DisplayMobile = () => {
     return (
-        <>
+      <>
+    <Grid item xs={12}>
+    <Typography variant="h2" component="h2" className={classes.headerAddress}>{realEstate?.address}</Typography>
+    <Typography variant="h6" component="h6">{realEstate?.city}</Typography>
+    <br/>
+    <Divider />
+    </Grid>
+  
+    <Grid item xs={12}>
+   <Typography variant="h4" component="h4" className={classes.headerAddress}>Oversikt</Typography>
+   
+    </Grid>
+
+    <Grid container item xs={12} spacing={1} className={classes.container}>
+          
+          <Grid item xs={12}>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Månedsleie: <span className={classes.secondInformationTextMobile}>{realEstate?.price}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Depositum: <span className={classes.secondInformationTextMobile}>{realEstate?.deposit}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Husleiegaranti: <span className={classes.secondInformationText}>{realEstate?.rentGuarantee}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Mulighet for parkering: <span className={classes.secondInformationTextMobile}>{realEstate?.parking}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Kvadratmeter: <span className={classes.secondInformationTextMobile}>{realEstate?.squareMeter}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Antall soverom: <span className={classes.secondInformationTextMobile}>{realEstate?.bedrooms}</span> </Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>Antall bad: <span className={classes.secondInformationTextMobile}>{realEstate?.bathrooms}</span> </Typography>
+          <br/>
+          <Divider/>
+          </Grid>
+          
+          <Grid container item xs={12} className={classes.container}>
+          
+          <Grid item xs={12}>
+          <Typography variant="h2" component="h2" className={classes.headerAddress}>{realEstate?.title}</Typography>
+          <Typography variant="h6" component="h6" className={classes.informationText}>{realEstate?.description}</Typography>
+          <br/>
+          <Divider/>
+          </Grid>
+         
+          </Grid>
+         
+          
+        <Grid item xs={12}>
+        <Card className={classes.cardContentMobile}>
+        
+      <CardContent>
+      <Typography
+        gutterBottom
+        variant="h4"
+        component="h2"
+        
+      >Kontakt oss om leilighet</Typography>
+      <br/>
+
+  
+      <br/>
+      <Button className={classes.button}>Send oss en mail</Button>
+      <br/>
+      <Button className={classes.buttonRediger}>Rediger annonse</Button>
+      <br/>
+      <Button className={classes.buttonSlett}>Slett annonse</Button>
+
+      </CardContent>
+    
+     </Card>
+          </Grid>
+          </Grid>
+          <Divider/>
+
       
+       
+       
+
+
+       </>
+    );
+  };  
+  
+    const DisplayDesktop = () => {
+      return (
+        <>
       <Grid item xs={12}>
       <Typography variant="h2" component="h2" className={classes.headerAddress}>{realEstate?.address}</Typography>
       <Typography variant="h6" component="h6">{realEstate?.city}</Typography>
@@ -180,7 +268,7 @@ const ApartmentView = () => {
         >Kontakt oss om leilighet</Typography>
         <br/>
 
-      <Typography variant="h6" component="h6" className={classes.informationText}>Månedsleie: <span className={classes.secondInformationText}>{realEstate?.price}</span> </Typography>
+     
         <br/>
         <Button className={classes.button}>Send oss en mail</Button>
         <br/>
@@ -196,7 +284,6 @@ const ApartmentView = () => {
             <Divider/>
 
         
-         
           <Grid container item xs={12} className={classes.container}>
             
             <Grid item xs={6}>
@@ -208,16 +295,29 @@ const ApartmentView = () => {
             </Grid>
 
 
-    </>
-  );
-};
+         </>
+      );
+    };
+    
+    const mobileView = useMediaQuery(theme.breakpoints.up("xs"));
+    const desktopView = useMediaQuery(theme.breakpoints.up("lg"));
+  
 
   return (
     <div className={classes.root}>
       <Container>
       <Grid container >
         <Grid container xs={12}>
-          <CornRow />
+        
+        {desktopView ? (
+         
+         <DisplayDesktop></DisplayDesktop>
+          
+        ) : mobileView ? (
+         
+        <DisplayMobile></DisplayMobile>
+        
+        ): <DisplayMobile/>}
         </Grid>
       </Grid>
       </Container>
