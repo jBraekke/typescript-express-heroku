74903e65ced3a669168c2bdc6f496dbe8a81e9eb
Dundyne <55193759+Dundyne@users.noreply.github.com>
2021-03-25 17:56:33 +0100

    fixed adduser

--- react-app/src/components/forms/AddUser.tsx
+++ react-app/src/components/forms/AddUser.tsx
@@ -4,7 +4,7 @@ import { Button, Card, MenuItem, Select } from "@material-ui/core";
 import { Controller, useForm } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,13 +18,16 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   formCard: {},
+  error: {
+    color: "red",
+  },
 }));
 
 const AddUser = () => {
   const classes = useStyles();
   const methods = useForm();
   const [datas, setDatas] = useState("");
-  const { handleSubmit, control } = methods;
+  const { handleSubmit, control, errors } = methods;
 
   async function postData(url = "", data = {}) {
     // Default options are marked with *
@@ -48,85 +51,136 @@ const AddUser = () => {
   const onSubmit = (data: any, e: any) => {
     setDatas("sending" + data.status);
     console.log(e);
-    postData("api/auth/register", data).then((data) => {
-      setDatas("sending" + data.status);
-      if (data.status === 200) {
-        setDatas("User created!");
-      } else setDatas("Failed to create user...");
-    });
+    postData("api/auth/register", data)
+      .then((data) => {
+        setDatas("sending" + data.status);
+        if (!data.ok) {
+          throw data;
+        }
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        err.text().then((errorMessage: any) => {
+          var dd = JSON.parse(errorMessage);
+          setDatas(dd.data);
+        });
+      });
   };
 
   return (
     <>
-     
-        <Typography variant="h4" component="h2">
-          Legg til bruker
-        </Typography>
-        <Typography>{datas}</Typography>
-        <form
-          className={classes.root}
-          noValidate
-          autoComplete="off"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          
-          <Controller
-            as={TextField}
-            name="firstName"
-            control={control}
-            defaultValue=""
-            rows={2}
-            multiline
-            variant="outlined"
-            label="Skriv inn ditt fornavn..."
-            id="subject"
-          />
-          <Controller
-            as={TextField}
-            name="lastName"
-            control={control}
-            defaultValue=""
-            variant="outlined"
-            multiline
-            rows={2}
-            label="Skriv inn ditt etternavn..."
-            id="text"
-          />
-
+      <Typography variant="h4" component="h2">
+        Legg til bruker
+      </Typography>
+      <Typography>{datas}</Typography>
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <Controller
-            as={TextField}
-            name="email"
-            control={control}
-            defaultValue=""
-            variant="outlined"
-            multiline
-            rows={2}
-            label="Skriv inn din email..."
-            id="text"
-          />
-         
-         <Controller
-            as={TextField}
-            name="password"
-            control={control}
-            defaultValue=""
-            variant="outlined"
-            multiline
-            rows={2}
-            label="Skriv inn ditt passord..."
-            id="text"
-          />
-         
-         
-
-          <Button 
-          type="submit"  variant={"contained"}
-                  color = "primary"
-                  endIcon={<AddIcon></AddIcon>}
-                  size={"large"}> Opprett bruker 
-          </Button>
-        </form>
-  
+          as={TextField}
+          name="firstName"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: true,
+            pattern: {
+              value: /^[A-Za-z0-9_-]*$/,
+              message: "Bare bokstaver og nummer tillatt",
+            },
+          }}
+          error={!!errors.firstName}
+          rows={2}
+          multiline
+          variant="outlined"
+          label="Skriv inn ditt fornavn..."
+          id="subject"
+        />
+        {errors.firstName && (
+          <span className={classes.error}>{errors.firstName.message}</span>
+        )}
+        <Controller
+          as={TextField}
+          name="lastName"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: true,
+            pattern: {
+              value: /^[A-Za-z0-9_-]*$/,
+              message: "Bare bokstaver og nummer tillatt",
+            },
+          }}
+          error={!!errors.lastName}
+          variant="outlined"
+          multiline
+          rows={2}
+          label="Skriv inn ditt etternavn..."
+          id="text"
+        />
+        {errors.lastName && (
+          <span className={classes.error}>{errors.lastName.message}</span>
+        )}
+        <Controller
+          as={TextField}
+          name="email"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: true,
+            pattern: {
+              value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "Må være en gyldig email",
+            },
+          }}
+          error={!!errors.email}
+          variant="outlined"
+          multiline
+          rows={2}
+          label="Skriv inn din email..."
+          id="text"
+        />
+        {errors.email && (
+          <span className={classes.error}>{errors.email.message}</span>
+        )}
+        <Controller
+          as={TextField}
+          name="password"
+          control={control}
+          defaultValue=""
+          rules={{
+            required: true,
+            pattern: {
+              value: /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/,
+              message: "Passord ikke sterkt nok/Må være 6 bokstaver",
+            },
+          }}
+          error={!!errors.password}
+          variant="outlined"
+          multiline
+          rows={2}
+          label="Skriv inn ditt passord..."
+          id="text"
+        />
+        {errors.password && (
+          <span className={classes.error}>{errors.password.message}</span>
+        )}
+        <Button
+          type="submit"
+          variant={"contained"}
+          color="primary"
+          endIcon={<AddIcon></AddIcon>}
+          size={"large"}
+        >
+          {" "}
+          Opprett bruker
+        </Button>
+      </form>
     </>
   );
 };
